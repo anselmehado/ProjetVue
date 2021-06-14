54f1a4c15d4c1ed5a42d0ad274b9735f0398355a
hado anselme <anselmehado85@yahoo.com>
2021-06-14 11:08:27 +0100

    update

--- student.js
+++ student.js
@@ -3,10 +3,10 @@ new Vue({
   el: '#app',
   // data is an attribute (value) of the defined Vue.js instance
   data: {
-    // name: 'A mountain B ro',
-    // course: 'Web Engineer Course',
-    // acceptancePeriod: 'January 2019',
-    // defaultLastId: 3,
+    name: 'A mountain B ro',
+    course: 'Web Engineer Course',
+    acceptancePeriod: 'January 2019',
+    defaultLastId: 3,
     students: [
       { id: 1, name: 'Hiroyoshi Noro', course: 'Machine learning course', acceptancePeriod: 'January 2019' },
       { id: 2, name: 'Shuji Tominaga', course: 'Web Engineer Course', acceptancePeriod: 'November 2017' },
@@ -15,19 +15,21 @@ new Vue({
   },
   // In methods, describe the method you want to use for the instance of Vue.js
   methods: {
-    //addStudent: function() {
     addStudent: function() {
-        this.todos.push({
-        id: this.nextTodoId++,
-        title: this.newTodoText
-      })
+      this.students.push({
+        id: this.defaultLastId++,
+        name: this.name,
+        course: this.course,
+        acceptancePeriod: this.acceptancePeriod,
 
-         this.studentText = ''
+      });
+
+      this.name = '';
+      this.course = '';
+      this.acceptancePeriod ='';
 
         // AddStudent method in this function () {}
         // Write the process of (the function that the value entered in the input field and the ID value (the value of the last registered ID value + 1) newly appear)
-    //}
-
-
+    }
   }
 })
